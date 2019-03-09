7249c70a39885e07ca9a8722e2359b6afea534b4
jacobseiler <jseiler@swin.edu.au>
2019-03-09 22:54:00 +1100

    Created a small script that creates the required directories and fetches mini-millennium trees.

---
+++ first_run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Welcome the Semi Analytic Galaxy Evolution model!"
+echo ""
+
+# We're going to execute things assuming we're in the root directory.
+# Hence let's first ensure that we're actually there.
+current_dir=${PWD##*/}  # This is the current directory without the base-name.
+if [[ $current_dir != "sage-model" ]]; then
+   echo "This setup script should be run from inside the `sage-model` directory."
+   echo "Please `cd` there first and execute again."
+   exit 1
+fi
+
+echo "Making the 'output/millennium' directory to hold the model output."
+
+# By using `-p`, `mkdir` won't fail if the directory already exists.
+mkdir -p output/millennium
+echo "Done."
+echo ""
+
+echo "Creating the 'input/millennium/trees' directory."
+mkdir -p input/millennium/trees
+echo "Done."
+echo ""
+
+cd input/millennium/trees
+
+if [ ! -f trees_063.7 ]; then
+    echo "Fetching Mini-Millennium trees."
+    wget "https://www.dropbox.com/s/l5ukpo7ar3rgxo4/mini-millennium-treefiles.tar?dl=0"  -O "mini-millennium-treefiles.tar"
+    if [[ $? != 0 ]]; then
+        echo "Could not download tree files from the Manodeep Sinha's Dropbox."
+        echo "Please open an issue on the 'sage-model' repository and we will assist ASAP :)"
+        exit 1
+    fi
+    echo "Done."
+    echo ""
+
+    tar xvf mini-millennium-treefiles.tar
+    if [[ $? != 0 ]]; then
+        echo "Could not untar the Mini-Millennium tree files."
+        echo "Please open an issue on the 'sage-model' repository and we will assist ASAP :)"
+        exit 1
+    fi
+    echo "Done."
+    echo ""
+
+    rm mini-millennium-treefiles.tar
+    echo "Mini-Millennium trees successfully gathered and placed into ${PWD}"
+else
+    echo "Mini-Millennium trees already present in 'input/millennium/trees'."
+fi
