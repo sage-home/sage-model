9e419defff3f67b8d8244be3b17e87a31d1a683d
jacobseiler <jseiler@swin.edu.au>
2019-03-20 11:05:58 +1100

    Accidentally decreased tolerance.

--- tests/sagediff.py
+++ tests/sagediff.py
@@ -421,8 +421,8 @@ if __name__ == '__main__':
     multidim_fields = ["Pos", "Vel", "Spin"]
 
     # Tolerance levels for the comparisons.
-    rtol = 1e-05
-    atol = 1e-08
+    rtol = 1e-07
+    atol = 1e-11
 
     print("")
     print("Running sagediff on files {0} and {1} in mode {2}".format(args.file1,
