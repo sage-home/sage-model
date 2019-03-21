626ef4ade828299a029a4cff967fda30730fb3db
jacobseiler <jseiler@swin.edu.au>
2019-03-21 17:22:03 +1100

    Manodeep has not failed me.

--- tests/install_hdf5.sh
+++ tests/install_hdf5.sh
@@ -1,6 +1,15 @@
-wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.19/src/hdf5-1.8.19.tar.bz2
-tar -xvjf hdf5-1.8.19.tar.bz2
-cd hdf5-1.8.19
-./configure --prefix=$HDF5_PREFIX --enable-shared --enable-parallel CC=mpicc
-make
-make install
+# Just a small script to install the parallel HDF5 library.
+
+if [[ -d $HDF5_DIR ]]; then
+    echo "Parallel HDF5 library not installed in cache directory ${HDF5_DIR}."
+    echo "Downloading and building."
+
+    wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.19/src/hdf5-1.8.19.tar.bz2
+    tar -xvjf hdf5-1.8.19.tar.bz2
+    cd hdf5-1.8.19
+    ./configure --prefix=$HDF5_DIR --enable-shared --enable-parallel CC=mpicc
+    make
+    make install
+else
+    echo "Parallel HDF5 library already installed in cache directory ${HDF5_DIR}."
+fi
