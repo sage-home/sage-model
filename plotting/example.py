723de851bb2a9b13ccee74b2f296da76749bfeea
Darren Croton <darrencroton@icloud.com>
2024-05-31 14:45:23 +1000

    Updated with plot_helper to allow saving figures to custom locations (via line 7)

--- plotting/example.py
+++ plotting/example.py
@@ -3,9 +3,13 @@ from pathlib import Path
 from sage_analysis.galaxy_analysis import GalaxyAnalysis
 from sage_analysis.default_analysis_arguments import default_plot_toggles
 
+from sage_analysis.plot_helper import PlotHelper
+plot_helper = PlotHelper(output_path="../output/millennium/plots/")
+
 if __name__ == "__main__":
 
     millennium_par_fname = Path(__file__).parent.joinpath("../input/millennium.par")
+    print(millennium_par_fname)
     par_fnames = [millennium_par_fname]
 
     plot_toggles = default_plot_toggles.copy()  # Copy to ensure ``default_plot_toggles`` aren't overwritten.
@@ -24,4 +28,4 @@ if __name__ == "__main__":
         },
     )
     galaxy_analysis.analyze_galaxies()
-    galaxy_analysis.generate_plots()
+    galaxy_analysis.generate_plots(plot_helper=plot_helper)
