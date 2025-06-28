2ce30632e108fcdcf2e3a8156ddab05c672c66b9
MBradley1985 <2496844@student.swin.edu.au>
2025-06-28 10:01:41 +1000

    CGM transfer is now fully controllable with parameters for the amount that goes through the CGM or Hot Gas directly, the speed per dynamical timescale the transfer from CGM to Hot Gas occurs at and the metallicity/pristeeness of the gas going into the Hot Gas from the CGM. Next to control the mixing that happens, the timescale that mixing of metals and pristine gas occurs at.

--- src/model_inflow.c
+++ src/model_inflow.c
@@ -65,64 +65,52 @@ void transfer_cgm_to_hot(const int centralgal, const double dt, struct GALAXY *g
 {
     if(galaxies[centralgal].CGMgas <= 0.0) return;
     
-    // Calculate transfer rate following the same pattern as inflow_gas
-    const double Vcrit = 445.48 * run_params->CGMTransferEfficiency;
-
-    // Debug output for central galaxy
-    // if(centralgal == 0) {
-    //     printf("DEBUG Transfer: Vvir=%.1f, Vcrit=%.1f, CGMgas=%.6f, Rvir=%.6f\n",
-    //            galaxies[centralgal].Vvir, Vcrit, galaxies[centralgal].CGMgas, galaxies[centralgal].Rvir);
-    // }
+    // Calculate proper SAGE dynamical timescale using disk radius
+    // reff = 3.0 * DiskScaleRadius = (3.0/√2) × λ × Rvir  
+    const double reff = 3.0 * galaxies[centralgal].DiskScaleRadius;
+    const double dynamical_time = reff / galaxies[centralgal].Vvir;
     
-    if(galaxies[centralgal].Vvir > Vcrit) {
-        // Base transfer rate similar to reincorporation calculation
-        double base_transfer_rate = 
-            (galaxies[centralgal].Vvir / Vcrit - 1.0) * 
-            galaxies[centralgal].CGMgas / (galaxies[centralgal].Rvir / galaxies[centralgal].Vvir);
+    // Transfer rate: fraction of CGM per dynamical time
+    double base_transfer_rate = run_params->CGMTransferEfficiency * 
+                               galaxies[centralgal].CGMgas / dynamical_time;
             
-        double transfer_amount = base_transfer_rate * dt;
+    double transfer_amount = base_transfer_rate * dt;
+    
+    if(transfer_amount > galaxies[centralgal].CGMgas) {
+        transfer_amount = galaxies[centralgal].CGMgas;
+    }
+    
+    if(transfer_amount > 0.0) {
+        // Calculate fractions to transfer
+        double pristine_fraction = 0.0;
+        double enriched_fraction = 0.0;
         
-        if(transfer_amount > galaxies[centralgal].CGMgas) {
-            transfer_amount = galaxies[centralgal].CGMgas;
+        if(galaxies[centralgal].CGMgas > 0.0) {
+            pristine_fraction = galaxies[centralgal].CGMgas_pristine / galaxies[centralgal].CGMgas;
+            enriched_fraction = galaxies[centralgal].CGMgas_enriched / galaxies[centralgal].CGMgas;
         }
-
-        // if(centralgal == 0 && transfer_amount > 0.0) {
-        //     printf("DEBUG Transfer: base_rate=%.6f, transfer_amount=%.6f\n", 
-        //            base_transfer_rate, transfer_amount);
-        // }
         
-        if(transfer_amount > 0.0) {
-            // Calculate fractions to transfer
-            double pristine_fraction = 0.0;
-            double enriched_fraction = 0.0;
-            
-            if(galaxies[centralgal].CGMgas > 0.0) {
-                pristine_fraction = galaxies[centralgal].CGMgas_pristine / galaxies[centralgal].CGMgas;
-                enriched_fraction = galaxies[centralgal].CGMgas_enriched / galaxies[centralgal].CGMgas;
-            }
-            
-            double pristine_transfer = transfer_amount * pristine_fraction;
-            double enriched_transfer = transfer_amount * enriched_fraction;
-            
-            // Calculate metallicity of transferred gas
-            const double metallicity = get_metallicity(galaxies[centralgal].CGMgas, 
-                                                      galaxies[centralgal].MetalsCGMgas);
-            
-            // Transfer from CGM to hot
-            galaxies[centralgal].CGMgas -= transfer_amount;
-            galaxies[centralgal].CGMgas_pristine -= pristine_transfer;
-            galaxies[centralgal].CGMgas_enriched -= enriched_transfer;
-            galaxies[centralgal].MetalsCGMgas -= metallicity * transfer_amount;
-            
-            galaxies[centralgal].HotGas += transfer_amount;
-            galaxies[centralgal].MetalsHotGas += metallicity * transfer_amount;
+        double pristine_transfer = transfer_amount * pristine_fraction;
+        double enriched_transfer = transfer_amount * enriched_fraction;
+        
+        // Calculate metallicity of transferred gas
+        const double metallicity = get_metallicity(galaxies[centralgal].CGMgas, 
+                                                  galaxies[centralgal].MetalsCGMgas);
+        
+        // Transfer from CGM to hot
+        galaxies[centralgal].CGMgas -= transfer_amount;
+        galaxies[centralgal].CGMgas_pristine -= pristine_transfer;
+        galaxies[centralgal].CGMgas_enriched -= enriched_transfer;
+        galaxies[centralgal].MetalsCGMgas -= metallicity * transfer_amount;
+        
+        galaxies[centralgal].HotGas += transfer_amount;
+        galaxies[centralgal].MetalsHotGas += metallicity * transfer_amount;
 
-            // NEW: Track CGM to hot transfer
-            galaxies[centralgal].TransferRate_CGM_to_Hot += transfer_amount;
-            
-            // Safety checks
-            if(galaxies[centralgal].CGMgas_pristine < 0.0) galaxies[centralgal].CGMgas_pristine = 0.0;
-            if(galaxies[centralgal].CGMgas_enriched < 0.0) galaxies[centralgal].CGMgas_enriched = 0.0;
-        }
+        // Track CGM to hot transfer
+        galaxies[centralgal].TransferRate_CGM_to_Hot += transfer_amount;
+        
+        // Safety checks
+        if(galaxies[centralgal].CGMgas_pristine < 0.0) galaxies[centralgal].CGMgas_pristine = 0.0;
+        if(galaxies[centralgal].CGMgas_enriched < 0.0) galaxies[centralgal].CGMgas_enriched = 0.0;
     }
 }
