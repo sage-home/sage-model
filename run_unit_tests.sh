54bffb303818a7969512b57cd7432bd7b573904b
Darren Croton <2062219+darrencroton@users.noreply.github.com>
2025-08-19 17:18:54 +1000

    Ingested run_unit_tests.sh into cmake

--- run_unit_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Simple script to run unit tests
-echo ""
-echo "=== Running All Unit Tests ==="
-/opt/homebrew/bin/ctest --output-on-failure -E "sage_end_to_end" || echo "Some unit tests failed (may be expected during development)"
