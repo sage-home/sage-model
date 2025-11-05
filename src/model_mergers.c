931951cca7c7931d9432d6a06e523e7156326206
MBradley1985 <2496844@student.swin.edu.au>
2025-11-05 20:38:17 +1100

    ensure NaNs are handled properly

--- src/model_mergers.c
+++ src/model_mergers.c
@@ -269,18 +269,23 @@ void collisional_starburst_recipe(const double mass_ratio, const int merger_cent
             const double vc = galaxies[merger_centralgal].Vvir;
             const double V_CRIT = 60.0;
             
-            double z_term = pow(1.0 + z, run_params->RedshiftPowerLawExponent);
-            double v_term;
-            if (vc < V_CRIT) {
-                v_term = pow(vc / V_CRIT, -3.2);
+            // Check for valid inputs to avoid NaN
+            if(vc <= 0.0 || z < 0.0) {
+                reheated_mass = 0.0;
             } else {
-                v_term = pow(vc / V_CRIT, -1.0);
+                double z_term = pow(1.0 + z, run_params->RedshiftPowerLawExponent);
+                double v_term;
+                if (vc < V_CRIT) {
+                    v_term = pow(vc / V_CRIT, -3.2);
+                } else {
+                    v_term = pow(vc / V_CRIT, -1.0);
+                }
+                double scaling_factor = z_term * v_term;
+                
+                // Reheating with Muratov scaling: η = 2.9 × (1+z)^α × (V/60)^β
+                double eta_reheat = run_params->FeedbackReheatingEpsilon * scaling_factor;
+                reheated_mass = eta_reheat * stars;
             }
-            double scaling_factor = z_term * v_term;
-            
-            // Reheating with Muratov scaling: η = 2.9 × (1+z)^α × (V/60)^β
-            double eta_reheat = run_params->FeedbackReheatingEpsilon * scaling_factor;
-            reheated_mass = eta_reheat * stars;
         } else {
             reheated_mass = run_params->FeedbackReheatingEpsilon * stars;
         }
@@ -308,28 +313,33 @@ void collisional_starburst_recipe(const double mass_ratio, const int merger_cent
                 const double vc = galaxies[merger_centralgal].Vvir;
                 const double V_CRIT = 60.0;
                 
-                double z_term = pow(1.0 + z, run_params->RedshiftPowerLawExponent);
-                double v_term;
-                if (vc < V_CRIT) {
-                    v_term = pow(vc / V_CRIT, -3.2);
-                } else {
-                    v_term = pow(vc / V_CRIT, -1.0);
-                }
-                double scaling_factor = z_term * v_term;
-                
-                // Total feedback energy: E_FB = ε_eject × scaling × 0.5 × M_* × (η_SN × E_SN)
-                double E_FB = run_params->FeedbackEjectionEfficiency * scaling_factor * 
-                              0.5 * stars * (run_params->EtaSNcode * run_params->EnergySNcode);
-                
-                // Energy needed to lift reheated gas to virial radius: E_lift = 0.5 × M_reheat × V_vir²
-                double E_lift = 0.5 * reheated_mass * vc * vc;
-                
-                // Leftover energy ejects additional gas: E_eject = E_FB - E_lift
-                // Ejected mass: M_eject = E_eject / (0.5 × V_vir²)
-                if(E_FB > E_lift) {
-                    ejected_mass = (E_FB - E_lift) / (0.5 * vc * vc);
-                } else {
+                // Check for valid inputs to avoid NaN
+                if(vc <= 0.0 || z < 0.0) {
                     ejected_mass = 0.0;
+                } else {
+                    double z_term = pow(1.0 + z, run_params->RedshiftPowerLawExponent);
+                    double v_term;
+                    if (vc < V_CRIT) {
+                        v_term = pow(vc / V_CRIT, -3.2);
+                    } else {
+                        v_term = pow(vc / V_CRIT, -1.0);
+                    }
+                    double scaling_factor = z_term * v_term;
+                    
+                    // Total feedback energy: E_FB = ε_eject × scaling × 0.5 × M_* × (η_SN × E_SN)
+                    double E_FB = run_params->FeedbackEjectionEfficiency * scaling_factor * 
+                                  0.5 * stars * (run_params->EtaSNcode * run_params->EnergySNcode);
+                    
+                    // Energy needed to lift reheated gas to virial radius: E_lift = 0.5 × M_reheat × V_vir²
+                    double E_lift = 0.5 * reheated_mass * vc * vc;
+                    
+                    // Leftover energy ejects additional gas: E_eject = E_FB - E_lift
+                    // Ejected mass: M_eject = E_eject / (0.5 × V_vir²)
+                    if(E_FB > E_lift) {
+                        ejected_mass = (E_FB - E_lift) / (0.5 * vc * vc);
+                    } else {
+                        ejected_mass = 0.0;
+                    }
                 }
             } else {
                 ejected_mass =
